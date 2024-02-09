244f728ec17b8a4d863c869b066a3511f6c9eb89
root <H.j.m.v.genuchten@student.tue.nl>
2024-02-09 15:48:57 +0000

    Move training loop into trainer

--- code/src/main.py
+++ code/src/main.py
@@ -4,33 +4,23 @@ from torchvision.transforms import v2 as transforms
 
 from data import SegmentationToyDataset
 from model import BinarySegmentationModel
+from trainer import Trainer
 
 def main():
     dataset = SegmentationToyDataset()
     dataloader = dataset.to_loader()
 
     model = BinarySegmentationModel()
-    optimizer = optim.Adam(model.parameters())
     loss_fn = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters())
+    
+    trainer = Trainer(
+        dataloader, model, loss_fn, optimizer
+    )
 
-    pbar = trange(0, 100)
-    for epoch in pbar:
-        loss_sum = 0
-        loss_count = 0
-        for batch_idx, (img, target) in enumerate(tqdm(dataloader, leave=False)):
-            pred = model(img)
-            loss = loss_fn(pred, target)
-
-            # Backpropagation
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-
-            # Keep track of average loss
-            loss_d = loss.detach()
-            loss_sum += loss_d.sum()
-            loss_count += img.shape[0]
-        pbar.set_description(f"avg_loss: {loss_sum/loss_count}")
+    for epoch in (pbar := trange(0, 100)):
+        loss = trainer.epoch()
+        pbar.set_description(f"avg_loss: {loss}")
 
 
 
--- code/src/trainer.py
+++ code/src/trainer.py
@@ -1,5 +1,5 @@
-from torch import nn
-
+from torch import nn, optim
+from tqdm import tqdm
 
 class Trainer:
     def __init__(
@@ -7,10 +7,28 @@ class Trainer:
         dataloader: nn.Module,
         model: nn.Module,
         loss_fn: nn.Module,
+        optimizer: optim.Optimizer
     ) -> None:
         self.dataloader = dataloader
         self.model = model
         self.loss_fn = loss_fn
+        self.optimizer = optimizer
 
     def epoch(self):
-        pass
+        loss_sum = 0
+        loss_count = 0
+        for batch_idx, (img, target) in enumerate(tqdm(self.dataloader, leave=False)):
+            pred = self.model(img)
+            loss = self.loss_fn(pred, target)
+
+            # Backpropagation
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+            # Keep track of average loss
+            loss_d = loss.detach()
+            loss_sum += loss_d.sum()
+            loss_count += img.shape[0]
+
+        return loss_sum / loss_count
