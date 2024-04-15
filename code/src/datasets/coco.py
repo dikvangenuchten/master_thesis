Fix backprop of stochastic layers

code/src/datasets/coco.py
@@ -112,7 +112,7 @@ class CoCoDataset(torch.utils.data.Dataset):
             dist = DiagonalGaussianDistribution(
                 parameters=parameters.unsqueeze(0)
             )
-            return LatentTensor(dist.sample().squeeze(0))
+            return LatentTensor(dist.rsample().squeeze(0))
         else:
             mean, _logvar = parameters.chunk(parameters, 2, dim=0)
             return LatentTensor(mean)

code/src/models/semantic_vae.py
@@ -285,17 +285,20 @@ class DecoderBlock(nn.Module):
         self._prior_net = ResBlock(
             in_channels=skip_channels, out_channels=skip_channels
         )
-        self._prior_layer = SampleConvLayer(skip_channels, latent_channels)
+        self._prior_layer = SampleConvLayer(
+            skip_channels, latent_channels
+        )
 
         self._posterior_net = ResBlock(
             in_channels=skip_channels + skip_channels,
             out_channels=skip_channels,
         )
-        self._posterior_layer = SampleConvLayer(skip_channels, latent_channels)
-        
+        self._posterior_layer = SampleConvLayer(
+            skip_channels, latent_channels
+        )
+
         self._z_projection = nn.Sequential(
-            nn.Conv2d(latent_channels, skip_channels, 1, 1),
-            nn.SiLU()
+            nn.Conv2d(latent_channels, skip_channels, 1, 1), nn.SiLU()
         )
 
         self._out_resblock = ResBlock(
@@ -342,8 +345,8 @@ class DecoderBlock(nn.Module):
         if not self.training:
             z = dist.scale
         else:
-            # z = dist.sample()
-            z = dist.scale
+            z = dist.rsample()
+            # z = dist.scale
 
         z_proj = self._z_projection(z)
         out["out"] = self._out_resblock(residual + z_proj)