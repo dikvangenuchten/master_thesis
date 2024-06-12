36c2e440acbc8f39db46057a5b67576759a90f27
Dik van Genuchten <H.j.m.v.genuchten@student.tue.nl>
2024-06-12 16:14:55 +0000

    Create sbatch script that can clean my folder
    Former-commit-id: a0acc3276e4c8b6ac9a980f5026ad70eb983a093

---
+++ code/clean_local.sh
@@ -0,0 +1,12 @@
+#SBATCH --job-name=master-thesis-dik
+#SBATCH --output=results/masther-thesis_%j.txt
+#SBATCH --partition=mcs.gpu.q
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gpus=0
+#SBATCH --gres=gpu:1
+
+rm -r /local/20182591/
