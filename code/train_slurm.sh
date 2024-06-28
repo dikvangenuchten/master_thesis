45b648283bb0469e4c93a7a24b48d93e61dfa425
Dik van Genuchten <H.j.m.v.genuchten@student.tue.nl>
2024-06-28 13:56:23 +0000

    Use more cpu cores on slurm
    Former-commit-id: 064f205771c6d8b53ce56c29ae2706d777648a77

--- code/train_slurm.sh
+++ code/train_slurm.sh
@@ -6,8 +6,8 @@
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=7
+#SBATCH --mem-per-cpu=8G
 #SBATCH --gpus=1
 #SBATCH --gres=gpu:1
 
