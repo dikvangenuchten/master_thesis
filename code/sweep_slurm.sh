4a32345a7e55b72101cbccd8a20ece1648b9668c
Dik van Genuchten <H.j.m.v.genuchten@student.tue.nl>
2024-07-02 09:39:13 +0000

    Fix
    Former-commit-id: 7d28a5f04d1a06e0e471217b7756d907b789ddb9

--- code/sweep_slurm.sh
+++ code/sweep_slurm.sh
@@ -4,8 +4,8 @@ start=`date +%s`
 module purge
 module load Python/3.10.13-GCCcore-11.3.0
 python -m venv venv
-python -m pip install -r requirements_d.txt
-python -m pip install -r requirements.txt
+python -m pip install -qr requirements_d.txt
+python -m pip install -qr requirements.txt
 end=`date +%s`
 echo Installing pacakges took: `expr $end - $start` seconds.
 
@@ -14,4 +14,4 @@ echo "Starting Python script"
 set -o allexport
 source .env
 set +o allexport
-python src/hydra_main.py -m +mod=tue hydra/launcher=tue_slurm "@"
+python src/hydra_main.py -m +mod=tue hydra/launcher=tue_slurm "$@"
