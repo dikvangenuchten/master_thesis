112310fb221d52a6a48b272136051e4ea9953109
Dik van Genuchten <H.j.m.v.genuchten@student.tue.nl>
2024-07-02 09:35:42 +0000

    Add script to run slurm sweep
    Former-commit-id: 114dea91136e982b2948fcda2ab0a5bf844098b1

---
+++ code/sweep_slurm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+start=`date +%s`
+module purge
+module load Python/3.10.13-GCCcore-11.3.0
+python -m venv venv
+python -m pip install -r requirements_d.txt
+python -m pip install -r requirements.txt
+end=`date +%s`
+echo Installing pacakges took: `expr $end - $start` seconds.
+
+# Starting sweep
+echo "Starting Python script"
+set -o allexport
+source .env
+set +o allexport
+python hydra_main.py -m +mod=tue hydra/launcher=tue_slurm "@"
