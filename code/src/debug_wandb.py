3a88ae063c1948951ae322d0d632253428164650
Dik van Genuchten <H.j.m.v.genuchten@student.tue.nl>
2024-07-22 20:50:58 +0200

    Fix error in debug script

--- code/src/debug_wandb.py
+++ code/src/debug_wandb.py
@@ -11,11 +11,16 @@ logging.info("Imported: DictConfig")
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def hydra_entrypoint(cfg: DictConfig) -> None:
-    logging.warning("Started in entrypoint")
-    import wandb
-    logging.warning("Imported wandb")
-    wandb.init(project_name="MasterThesis-debug")
-    logging.info("Initialized wandb")
+    try:
+        logging.warning("Started in entrypoint")
+        import wandb
+        logging.warning("Imported wandb")
+        wandb.sdk.wandb_init._set_logger(logging.getLogger())
+        logging.warning("set wandb logger")
+        wandb.init("MasterThesis-debug")
+        logging.warning("Initialized wandb")
+    except Exception as e:
+        logging.exception("Got exception", exc_info=e)
 
 
 if __name__ == "__main__":
