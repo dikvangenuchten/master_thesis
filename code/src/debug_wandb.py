084c98b497ef31abcb2a56245262870b23103a5d
Dik van Genuchten <H.j.m.v.genuchten@student.tue.nl>
2024-07-22 20:54:19 +0200

    More minimal

--- code/src/debug_wandb.py
+++ code/src/debug_wandb.py
@@ -2,18 +2,19 @@
 """
 
 import logging
+
 logging.info("Imported: logging")
 import hydra
+
 logging.info("Imported: hydra")
-from omegaconf import DictConfig
-logging.info("Imported: DictConfig")
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
-def hydra_entrypoint(cfg: DictConfig) -> None:
+def hydra_entrypoint(cfg) -> None:
     try:
         logging.warning("Started in entrypoint")
         import wandb
+
         logging.warning("Imported wandb")
         wandb.sdk.wandb_init._set_logger(logging.getLogger())
         logging.warning("set wandb logger")
