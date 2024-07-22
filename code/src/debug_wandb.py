29ed60ee8472bf765e9da427c18aa87cb8c41182
Dik van Genuchten <H.j.m.v.genuchten@student.tue.nl>
2024-07-22 20:21:51 +0200

    Even more minimalist

--- code/src/debug_wandb.py
+++ code/src/debug_wandb.py
@@ -3,8 +3,6 @@
 
 import logging
 logging.info("Imported: logging")
-from accelerate import Accelerator
-logging.info("Imported: Accelerator")
 import hydra
 logging.info("Imported: hydra")
 from omegaconf import DictConfig
@@ -13,17 +11,11 @@ logging.info("Imported: DictConfig")
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def hydra_entrypoint(cfg: DictConfig) -> None:
-    logging.warning("Started in logger")
-    log_with = "wandb"
-
-    accelerator = Accelerator(
-        log_with=log_with,
-    )
-    logging.info("Created accelerator")
-    accelerator.init_trackers(
-        project_name="MasterThesis-debug",
-    )
-    logging.info("Initialized trackers")
+    logging.warning("Started in entrypoint")
+    import wandb
+    logging.warning("Imported wandb")
+    wandb.init(project_name="MasterThesis-debug")
+    logging.info("Initialized wandb")
 
 
 if __name__ == "__main__":
