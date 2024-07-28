9e69066dbfce932d425bcf02de4983b5591aaa60
Dik van Genuchten <H.j.m.v.genuchten@student.tue.nl>
2024-07-28 08:24:14 +0000

    Only save model to save storage space on the server

--- code/src/trainer.py
+++ code/src/trainer.py
@@ -271,25 +271,25 @@ class Trainer:
 
     def save(self, dir) -> None:
         os.makedirs(dir, exist_ok=True)
-
+        # Currently only saving model to save storage space
+        torch.save(
+            self.model.state_dict(), os.path.join(dir, "model.pt")
+        )
         # torch.save(
         #     self.train_dataloader,
         #     os.path.join(dir, "train_dataloader.pt"),
         # )
-        torch.save(
-            self.model.state_dict(), os.path.join(dir, "model.pt")
-        )
-        torch.save(
-            self.loss_fn.state_dict(), os.path.join(dir, "loss_fn.pt")
-        )
-        torch.save(
-            self.optimizer.state_dict(),
-            os.path.join(dir, "optimizer.pt"),
-        )
-        torch.save(
-            self.scheduler.state_dict(),
-            os.path.join(dir, "scheduler.pt"),
-        )
+        # torch.save(
+        #     self.loss_fn.state_dict(), os.path.join(dir, "loss_fn.pt")
+        # )
+        # torch.save(
+        #     self.optimizer.state_dict(),
+        #     os.path.join(dir, "optimizer.pt"),
+        # )
+        # torch.save(
+        #     self.scheduler.state_dict(),
+        #     os.path.join(dir, "scheduler.pt"),
+        # )
         # torch.save(
         # self.eval_dataloader,
         # os.path.join(dir, "eval_dataloader.pt"),
